a2f23fd9525ed89ac5e2e8f0ee47b27525cb8c36
Yashwin Sharma <yashwinsharma16@gmail.com>
2025-12-24 18:24:20 +0530

    Authentication

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,10 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { useEffect } from "react";
 import type { Route } from "./+types/root";
 import "./app.css";
+import { usePuterStore } from "./lib/puter";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +26,10 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const {init}=usePuterStore();
+  useEffect(()=>{
+    init()
+  },[init]);
   return (
     <html lang="en">
       <head>
@@ -32,7 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body><script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your Dream Job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  },[auth.isAuthenticated])
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    
+
+    {resumes.length>0 && (
+      <div className="resumes-section">
+        {resumes.map((resume)=>{
+      return(
+      <ResumeCard key={resume.id} resume={resume}/>
+      );
+    })}
+      </div>
+    )}
+    </section>
+
+  </main>;
 }
