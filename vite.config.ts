fa997b76a9f2d6c2fcb6b924f8eb30cfe1085421
Yashwin Sharma <yashwinsharma16@gmail.com>
2025-12-28 12:46:02 +0530

    vite commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  optimizeDeps: { exclude: ["pdfjs-dist"] },
-  assetsInclude: ["**/*.mjs"],
+  optimizeDeps: {
+    exclude: ["pdfjs-dist"], // prevent pre-bundling
+  },
+  ssr: {
+    noExternal: ["pdfjs-dist"], // ensures Vite skips server bundling
+  },
+  assetsInclude: ["**/*.mjs"], // allows PDF worker .mjs file to be bundled
 });
